7fc45b3d69e18048ad7e53170b3ac47f098eca48
peer2021 <85537435+peer2021@users.noreply.github.com>
2022-06-14 09:04:32 +0800

    Update dags_example_python_dtap-k8s.py

--- dags_example_python_dtap-k8s.py
+++ dags_example_python_dtap-k8s.py
@@ -30,7 +30,7 @@ start = DummyOperator(task_id='run_this_first', dag=dag)
 python_task = KubernetesPodOperator(namespace='test',
                                     image="devuser2021/sample-docker:latest",
                                     cmds=["python3", "/bd-fs-mnt/TenantShare/repo/code/test.py" ,"-c"],
-                                    labels={"hpecp.hpe.com/dtap": "hadoop2","hpecp.hpe.com/fsmount: test"},
+                                    labels={"hpecp.hpe.com/dtap": "hadoop2","hpecp.hpe.com/fsmount": "test"},
                                     resources={'limit_memory': "4Gi", 'limit_cpu': "500m"},
                                     name="passing-python",
                                     in_cluster=True,
